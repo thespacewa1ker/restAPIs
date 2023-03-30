8b39df429ced76f2886b905b579962c4f635e31f
Sanjay <47817778+thespacewa1ker@users.noreply.github.com>
2023-03-30 21:09:11 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const userController = require("../controllers/UserController");
+const userController = require("../controllers/userController.js")
 const { auth } = require("../middleware/auth");
 const saveController = require("../controllers/saveController");
 const questionsResponse = require("../models/questionsResponse");
