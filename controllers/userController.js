28d77d467b14ca99ba19eee0225902ca65c911b4
Sanjay <47817778+thespacewa1ker@users.noreply.github.com>
2023-03-30 21:52:51 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,6 +5,18 @@ module.exports = {
   register: async (req, res) => {
     try {
       const { name, email, password } = req.body;
+
+      // Check if user already exists
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        return res.status(400).json({ message: "User already exists" });
+      }
+
+      // Validate name, email, and password fields
+      if (!name || !email || !password) {
+        return res.status(400).json({ message: "Please fill in all fields" });
+      }
+      // Create new user
       const user = await User.create({ name, email, password });
       res.json(user);
     } catch (err) {
@@ -15,14 +27,20 @@ module.exports = {
   login: async (req, res) => {
     try {
       const { email, password } = req.body;
+
+      // Check if user exists
       const user = await User.findOne({ email });
       if (!user) {
-        return res.status(401).json({ message: "Invalid credentials" });
+        return res.status(401).json({ message: "User does not exist please register" });
       }
+
+      // Validate password
       const isMatch = await user.isValidPassword(password);
       if (!isMatch) {
         return res.status(401).json({ message: "Invalid credentials" });
       }
+
+      // Create and send JWT token
       const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
         expiresIn: "1d",
       });
